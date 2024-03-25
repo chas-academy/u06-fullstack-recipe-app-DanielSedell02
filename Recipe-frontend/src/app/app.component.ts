9b749173074031d5f3fa0709117cb36cd5011f97
DanielSedell02 <danne.sedell@gmail.com>
2024-03-25 20:08:12 +0100

    Login+Logout+Getuser done

--- Recipe-frontend/src/app/app.component.ts
+++ Recipe-frontend/src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
+import { User } from './interfaces/user';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Recipe-frontend';
+  title = 'authapp';
+
+  loginDetails: LoginDetails;
+
+  user: User;
+
+  loggedIn$: Observable<boolean>;
+
+  constructor(private auth: AuthService){
+    this.loginDetails = {
+      email:"seb@seb.seb",
+      password:"sebsebseb"
+    }
+
+    this.user = {
+      id:-1,
+      name:"",
+      email:""
+    }
+
+    this.loggedIn$ = this.auth.loggedIn$;
+  }
+
+  login(){
+    this.auth.loginUser(this.loginDetails);
+  }
+  logout(){
+    this.auth.logOut();
+  }
+
+  getUser(){
+    this.auth.getUser2().subscribe(res => {
+      console.log(res[0]);
+      this.user = res[0];
+    })
+  }
 }
