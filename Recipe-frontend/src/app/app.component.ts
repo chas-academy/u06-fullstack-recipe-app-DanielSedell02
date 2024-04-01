5cfdb038f00277bbeaae51c5d5512dcb9053bf0a
DanielSedell02 <danne.sedell@gmail.com>
2024-04-01 16:02:33 +0200

    Login,Logout + register works

--- Recipe-frontend/src/app/app.component.ts
+++ Recipe-frontend/src/app/app.component.ts
@@ -1,53 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { LoginDetails } from './interfaces/login-details';
-import { User } from './interfaces/user';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'authapp';
-
-  loginDetails: LoginDetails;
-
-  user: User;
-
-  loggedIn$: Observable<boolean>;
-
-  constructor(private auth: AuthService){
-    this.loginDetails = {
-      email:"seb@seb.seb",
-      password:"sebsebseb"
-    }
-
-    this.user = {
-      id:-1,
-      name:"",
-      email:""
-    }
-
-    this.loggedIn$ = this.auth.loggedIn$;
-  }
-
-  login(){
-    this.auth.loginUser(this.loginDetails);
-  }
-  logout(){
-    this.auth.logOut();
-  }
-
-  getUser(){
-    this.auth.getUser2().subscribe(res => {
-      console.log(res[0]);
-      this.user = res[0];
-    })
-  }
+  
 }
