9466535df6ce71346041184257df987f1939d2ab
DanielSedell02 <danne.sedell@gmail.com>
2024-04-02 18:35:26 +0200

    created register.component

---
+++ Recipe-frontend/src/app/pages/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  email: string = "";
+  password: string = "";
+  name: string = "";
+  password_confirmation: string = "";
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('name', this.name);
+    formData.append('email', this.email);
+    formData.append('password', this.password);
+  }
+}
+
+
