8801f5219a3b9d8d9f663b92c23c2a5785c2b664
DanielSedell02 <danne.sedell@gmail.com>
2024-04-03 12:46:30 +0200

    component created + routing

--- Recipe-frontend/src/app/app.routes.ts
+++ Recipe-frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path:'profile', component:ProfileComponent},
     {path:'login', component:LoginComponent},
     {path: 'register' , component:RegisterComponent},
+    {path: 'home', component:HomepageComponent},
 
    
 ];
